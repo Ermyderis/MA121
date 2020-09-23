dc49faf1f6c6cd2786618af88ddeebe7e0700c8f
dovyd <dovydas0000@gmail.com>
2020-09-23 23:57:46 +0300

    Added button and TextView

--- app/src/main/java/com/example/ma1/MainActivity.java
+++ app/src/main/java/com/example/ma1/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.ma1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.textView);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+                                   @SuppressLint("SetTextI18n")
+                                   @Override
+                                   public void onClick(View v){
+                                       txt.setText("Button was clicked");
+                                   }
+
+                               }
+        );
     }
 }
